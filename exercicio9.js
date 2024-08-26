9a9a35ac92e41b62b5b7c7307cf20266ffb832e8
luann <lv483713@gmail.com>
2024-08-26 15:20:51 -0300

    Add files via upload

---
+++ exercicio9.js
@@ -0,0 +1,24 @@
+function CalcularPesoIdeal() {
+    var altura = document.getElementById('altura').value;
+    var sexo = document.getElementById('sexo').value;
+    (ParaHomens (72.7*h) -58);
+    (ParaMulheres (62.1*h) -44.7);
+    var Sexo = document.getElementById('sexo').value;
+    (ParaHomens (72.7*h) -58);
+    (ParaMulheres (62.1*h) -44.7);
+    document.getElementById('Resultado').innerText='O resultado é: '+ Altura + Sexo
+
+switch (vinhoInput) {
+    case 'masculino':
+        CalcularPesoIdeal = (72.7* h) -58;
+        break;
+    case 'feminino':
+        CalcularPesoIdeal = (62.1*h) -44.7;
+        break;
+        default:
+        CalcularPesoIdeal('Erro no cálculo do peso');
+        break;
+}
+document.getElementById('resultado').innerText = 'Seu peso idela é:' 
+ {CalcularPesoIdeal.toFixed(2)}`;
+}
